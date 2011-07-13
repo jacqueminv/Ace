6073aefe4ae5546c1b6c105ab86861d6a4c2082b
Valentin Jacquemin <jacqueminv@gmail.com>
2011-07-13 15:38:56 +0200

    Bottom right artifacts added thanks to the rotation transform

--- Ace/AceView.m
+++ Ace/AceView.m
@@ -7,6 +7,7 @@
 //
 
 #import "AceView.h"
+#import <math.h>
 
 @interface AceView ()
 
@@ -26,18 +27,38 @@
         
         _centerHeart = [[UIImageView alloc] initWithImage:self.heart];
         _centerHeart.center = CGPointMake(frame.origin.x + frame.size.width / 2, 
-                                       frame.origin.y + frame.size.height / 2);
+                                          frame.origin.y + frame.size.height / 2);
         
         _topLeftNumber = self.cardNumber;
-        _topLeftNumber.frame = CGRectMake(15, 30, 25, 25);
+        _topLeftNumber.frame = CGRectMake(frame.origin.x + 15, 
+                                          frame.origin.y + 30, 
+                                          25, 
+                                          25);
         _topLeftHeart = [[UIImageView alloc] initWithImage:self.heart];
-        _topLeftHeart.frame = CGRectMake(15, 60, 25, 25);
+        _topLeftHeart.frame = CGRectMake(frame.origin.x + 15, 
+                                         frame.origin.y + 60, 
+                                         25, 
+                                         25);
         
+        CGAffineTransform reverse = CGAffineTransformMakeRotation(M_PI);
         _bottomRightNumber = self.cardNumber;
+        _bottomRightNumber.transform = reverse;
+        _bottomRightNumber.frame = CGRectMake(frame.origin.x + frame.size.width - 40,
+                                              frame.origin.y + frame.size.height - 55,
+                                              25,
+                                              25);
+        _bottomRightHeart = [[UIImageView alloc] initWithImage:self.heart];
+        _bottomRightHeart.transform = reverse;
+        _bottomRightHeart.frame = CGRectMake(frame.origin.x + frame.size.width - 40,
+                                             frame.origin.y + frame.size.height - 85,
+                                             25,
+                                             25);
         
         
         [self addSubview:_topLeftNumber];
         [self addSubview:_topLeftHeart];
+        [self addSubview:_bottomRightNumber];
+        [self addSubview:_bottomRightHeart];
         [self addSubview:_centerHeart];
     }
     return self;
